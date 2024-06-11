e67f0324b1b01feb7c9066353c4cf6884b8dbaf7
Mikhail K <kapranov.m@gmail.com>
2024-06-11 16:49:40 +0400

    Same

--- run-openocd-build.sh
+++ run-openocd-build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 docker rmi $(docker images -qa -f 'label=boocd')
 #docker rmi $(docker images -qa -f 'dangling=true')
-#docker build --no-cache --target builder --progress=plain --tag openoc-build --label boocd .
+
+#docker build --no-cache --target builder --progress=plain --tag openocd --label boocd .
 docker build --no-cache --target app --progress=plain --tag openocd --label boocd .
 
 docker run --rm -it -v /dev:/dev -p 4444:4444 -p 6666:6666 openocd:latest
