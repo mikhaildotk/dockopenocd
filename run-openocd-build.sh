c4c48ee60550384e2b86d20563c4f34a1327a3a9
Mikhail K <kapranov.m@gmail.com>
2024-06-21 17:39:55 +0400

    Add git tag var

--- run-openocd-build.sh
+++ run-openocd-build.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+
+OOCD_LASTAG=`git ls-remote --refs --tags git://git.code.sf.net/p/openocd/code | cut --delimiter='/' --fields=3 |  tr '-' '~' | sort --version-sort | tail --lines=1`
 docker rmi $(docker images -qa -f 'label=boocd')
 #docker rmi $(docker images -qa -f 'dangling=true')
 
-#docker build --no-cache --target builder --progress=plain --tag openocd --label boocd .
-docker build --no-cache --target app --progress=plain --tag openocd --label boocd .
+docker build --no-cache --target builder --progress=plain --tag foobar/openocd:${OOCD_LASTAG} --label boocd .
+#docker build --no-cache --target app --progress=plain --tag foobar/openocd:0.12 --label boocd .
 
-docker run --rm -it -v /dev:/dev -p 4444:4444 -p 6666:6666 openocd:latest
+docker run --rm -it -v /dev:/dev -p 4444:4444 -p 6666:6666 foobar/openocd:${OOCD_LASTAG}
